58c843ad2cde5f80eb1f121d890f1a38e50599da
rafly <rafly.tjasmono@gmail.com>
2023-08-30 13:10:35 +0700

    clean up

--- js/script.js
+++ js/script.js
@@ -85,41 +85,7 @@ function validateAndDisplay() {
   }
 
 
-// let nameInput= document.getElementById('name-input')
 
-// document.getElementById('kirim').addEventListener('click', function() {
-//     const nama = nameInput.value
-
-//     if(name == '') {
-//         document.getElementById('error-name').innerHTML= 'nama tidak boleh kosong'
-//     } else{
-
-//     document.getElementById('name').innerHTML= nama
-// }   
-// })
-
-// function validateForm(){
-//    const name = document.forms['message-form']['full-name'].value;
-//    const birthDate = document.forms['message-form']['birth-date'].value;
-//    const gender = document.forms['message-form']['gender'].value;
-//    const messages = document.forms['message-form']['messages'].value;
-
-//    if (name == '' || gender=='' || messages=='') {
-//     alert("tidak boleh ada yang kosong");
-//     return false;
-//    }
-
-//    setSenderUI(name, birthDate, gender, messages);
-
-//    return false;
-// }
-
-// function setSenderUI(name, birthDate, gender, messages) {
-//     document.getElementById('sender-full-name').innerHTML= name;
-//     document.getElementById('sender-birth-date').innerHTML= birthDate;
-//     document.getElementById('sender-gender').innerHTML= gender;
-//     document.getElementById('sender-messages').innerHTML= messages;
-// }
 
 
 var slideIndex=1;
